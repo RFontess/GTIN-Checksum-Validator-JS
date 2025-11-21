2b9cdc30d8bd0cb5ed58e0a7a1e32dc1cb9bbc1b
Rafael Fontes <rft180407@gmail.com>
2025-11-20 22:22:24 -0300

    Update validação.js

--- validação.js
+++ validação.js
@@ -7,7 +7,37 @@
 class validEAN {
     constructor(cod) {
         this.cod = cod;
+        this.codArray = Array.from(this.cod).map(Number);
+    }
+
+    posicoesImpares(){
+        const numerosPosicaoImpar = this.codArray.filter(function callback(valor,index) {
+            if(!((index+1) % 2 === 0)){
+                return valor;
+            }
+        });
+        let somaImpar = 0;
         
+        numerosPosicaoImpar.forEach(function somar (valor){
+            somaImpar += valor;
+        });
+
+        return somaImpar;
+    }    
+
+    posicoesPares(){
+        const numerosPosicaoPar = this.codArray.filter(function callback(valor,index) {
+            if((index+1) % 2 === 0){
+                return valor;
+            }
+        });
+        let somaPar = 0;
+        
+        numerosPosicaoPar.forEach(function somar (valor){
+            somaPar += valor;
+        });
+
+        return somaPar*=3;
     }
 
     calcular(){
@@ -15,44 +45,20 @@ class validEAN {
     }
 
     validar(){
-        const codArray = Array.from(this.cod).map(Number);
-
-        function posicoesImpares(){
-            const numerosPosicaoImpar = codArray.filter(function callback(valor,index) {
-                if(!((index+1) % 2 === 0)){
-                    return valor;
-                }
-            });
-            let somaImpar = 0;
-            
-            numerosPosicaoImpar.forEach(function somar (valor){
-                somaImpar += valor;
-            });
-
-            return somaImpar;
-        }
         
-        function posicoesPares(){
-            const numerosPosicaoPar = codArray.filter(function callback(valor,index) {
-                if((index+1) % 2 === 0){
-                    return valor;
-                }
-            });
-            let somaPar = 0;
         
-            numerosPosicaoPar.forEach(function somar (valor){
-                somaPar += valor;
-            });
-
-            return somaPar*=3
-        }
-        const somaResultados = posicoesImpares() + posicoesPares();
-        console.log(somaResultados)
-        const arrayString = codArray.toString();
-        console.log(arrayString)
+        let somaResultados = this.posicoesImpares() + this.posicoesPares();
+        somaResultados = Array.from(somaResultados.toString().split(''));
+        const ultimoDigitoCriado = somaResultados[(somaResultados.length-1)];
+        const ultimoDigitoOriginal = codArray[(codArray.length-1)];
+        return ultimoDigitoCriado === ultimoDigitoOriginal ? 'Seu código de barras está correto' : 'Seu código de barras está incorreto';
+        
+        
+        //const arrayString = codArray.toString();
+        //console.log(arrayString)
     }
 }
 
-const cod1 = new validEAN('12345678912');
-cod1.validar()
+const cod1 = new validEAN('1234567891432');
+console.log(cod1.validar())
 console.log(cod1)
