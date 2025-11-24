a26d6c41bd012046aba3d45e2571f2107ed70df3
Rafael Fontes <rft180407@gmail.com>
2025-11-24 13:39:45 -0300

    feat: add digit quantity validation

--- main.js
+++ main.js
@@ -14,24 +14,48 @@ input.addEventListener('input', event => {
 function executarValidacao(){
     const valorInput = input.value
     if(!valorInput) return mostrarResultado('❌ Por favor, digite um código.', 'danger');
+    if(valorInput.length < 8) return mostrarResultado('❌ O código deve ter pelo menos 8 dígitos para ser validado.', 'danger');
     const validacao = new validEAN(valorInput);
-    const resultado = validacao.validar();
-
-    if(resultado){
+    const codigoValidado = validacao.validar();
+    
+    if(codigoValidado){
         return mostrarResultado(`✅ O código <strong>${valorInput}</strong> é VÁLIDO!`, 'success');
     }
     else{
         return mostrarResultado(`❌ O código <strong>${valorInput}</strong> é INVÁLIDO.`, 'danger');
     }
+    
 }
 
 function executarCalculo(){
     const valorInput = input.value
     if(!valorInput) return mostrarResultado('❌ Digite os números para calcular o próximo dígito.', 'danger');
+    if(valorInput.length < 7) return mostrarResultado('❌ O código deve ter pelo menos 7 dígitos para ser calculado.', 'danger');
     const calculo = new validEAN(valorInput);
-    const resultado = calculo.calcular().join('');
+    const codigoCalculado = calculo.calcular().join('');
 
-    return mostrarResultado(`Novo código gerado: <br><strong>${resultado}</strong>`, 'info');
+    let tipoCodigo; 
+
+    switch (true) {
+        case codigoCalculado.length === 8:
+            tipoCodigo = 'EAN-8'
+            break;
+        case codigoCalculado.length === 12:
+            tipoCodigo = 'UPC-A'
+            break;
+        case codigoCalculado.length === 13:
+            tipoCodigo = 'EAN-13'
+            break;
+        case codigoCalculado.length === 14:
+            tipoCodigo = 'GTIN-14'
+            break;
+        default:
+            tipoCodigo = ''
+            break;
+    }
+
+
+    return mostrarResultado(`Novo código ${tipoCodigo} gerado: <br><strong>${codigoCalculado}</strong>`, 'info');
 }
 
 function mostrarResultado(texto, tipo){
