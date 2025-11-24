08aa823f93a396b65bc76c6b4061201513e66ba6
Rafael Fontes <rft180407@gmail.com>
2025-11-23 23:44:02 -0300

    refactor

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 const input = document.getElementById('inputCodigo')
 const divResultado = document.getElementById('resultado');
+const botaoCalcular = document.getElementById('calcular')
+const botaoValidar = document.getElementById('validar')
+
+botaoCalcular.addEventListener('click', event => executarCalculo());
+botaoValidar.addEventListener('click', event => executarValidacao());
+
+input.addEventListener('input', event => {
+    if(!input.value){
+        limparResultado();
+    }
+})
+function executarValidacao(){
+    const valorInput = input.value
+    if(!valorInput) return mostrarResultado('❌ Por favor, digite um código.', 'danger');
+    const validacao = new validEAN(valorInput);
+    const resultado = validacao.validar();
+
+    if(resultado){
+        return mostrarResultado(`✅ O código <strong>${valorInput}</strong> é VÁLIDO!`, 'success');
+    }
+    else{
+        return mostrarResultado(`❌ O código <strong>${valorInput}</strong> é INVÁLIDO.`, 'danger');
+    }
+}
+
+function executarCalculo(){
+    const valorInput = input.value
+    if(!valorInput) return mostrarResultado('❌ Digite os números para calcular o próximo dígito.', 'danger');
+    const calculo = new validEAN(valorInput);
+    const resultado = calculo.calcular().join('');
+
+    return mostrarResultado(`Novo código gerado: <br><strong>${resultado}</strong>`, 'info');
+}
 
 function mostrarResultado(texto, tipo){
     divResultado.style.display = 'block';
@@ -7,25 +40,7 @@ function mostrarResultado(texto, tipo){
     divResultado.className = `mt-3 alert alert-${tipo}`;
 }
 
-function executarValidacao(){
-    const valorInput = input.value
-    if(!valorInput) return mostrarResultado('Por favor, digite um código.', 'danger');
-    const validacao = new validEAN(valorInput);
-    const resultado = validacao.validar();
-
-    if(resultado){
-        return mostrarResultado(`Seu código de barras está correto.`, 'success');
-    }
-    else{
-        return mostrarResultado(`Seu código de barras está incorreto.`, 'danger');
-    }
-}
-
-function executarCalculo(){
-    const valorInput = input.value
-    if(!valorInput) return mostrarResultado('Por favor, digite um código.', 'danger');
-    const calculo = new validEAN(valorInput);
-    const resultado = calculo.calcular();
-
-    return mostrarResultado(`Seu novo código de barras: ${resultado.join('')}`, 'success');
+function limparResultado(){
+    divResultado.innerHTML = '';
+    divResultado.className = '';
 }
